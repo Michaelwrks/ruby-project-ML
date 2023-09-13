082d39b3db963a2c77361891808156a2ea294b3c
Olaleye Oduola <olaoduola01@gmail.com>
2023-09-13 16:43:13 +0100

    user login complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_132208) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_151538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_132208) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
