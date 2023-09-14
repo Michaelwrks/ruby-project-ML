87a460fe15a10ca40b20ca596010f79d8d1e6d1f
Olaleye Oduola <olaoduola01@gmail.com>
2023-09-14 11:00:00 +0100

    logout button working

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
  get "signup", to: "users#new"
  get "login", to: "sessions#new"
  post "login", to: "sessions#create"
+ get 'logout', to: 'sessions#destroy', as: :logout
  resources :users, except: [:new]
 
 end
