268dc223dfca36beca030302d70a151467b1fde7
Olaleye Oduola <olaoduola01@gmail.com>
2023-09-20 11:53:50 +0100

    navbar redirect complete n got rid of <% on all pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
 
  get "signup", to: "users#new"
+ get 'about', to: 'home#about', as: :about
  get "login", to: "sessions#new"
  get "logged_in", to: "sessions#new"
  post "login", to: "sessions#create"
