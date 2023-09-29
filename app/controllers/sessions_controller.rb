8bad6c6ec5ddef9d15178ff51c1bd68098fce190
Michaelwrks <120025232+Michaelwrks@users.noreply.github.com>
2023-09-29 15:12:47 +0100

    latest-front-end-changespt2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,14 @@
 # app/controllers/sessions_controller.rb
 class SessionsController < ApplicationController
-  def create
-      user = User.find_by(username: params[:session][:username].downcase)
-      if user && user.authenticate(params[:session][:password])
-        session[:user_id] = user.id
-        flash[:notice] = "Logged in successfully."
-        redirect_to root_path
-      else
-        flash.now[:alert] = "There was something wrong with your login details."
-        render 'new'
+    def create
+        if user_authenticated
+            respond_to do |format|
+              format.js { render 'login_success' }
+            end
+          else
+            respond_to do |format|
+              format.js { render 'login_failure' }
+            end
+          end
+        end
       end
-    end
-  def logged
-    
-  end
-  def destroy
-    session[:user_id] = nil
-    flash[:notice] = "You have been logged out."
-    redirect_to root_path
-  end
-  
-end
